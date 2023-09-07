996cf6e40d6e94abe0f782f97b8278dadd910953
Samuel Amaro <samuelamaro96746313@gmail.com>
2023-09-07 11:35:15 -0300

    componente Icon

---
+++ src/components/Icons/ChevronDown.tsx
@@ -0,0 +1,16 @@
+import { PropsIcon } from "@/utils/utils";
+
+export default function ChevronDown({ className }: PropsIcon) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="14"
+      height="9"
+      fill="none"
+      viewBox="0 0 14 9"
+      className={className}
+    >
+      <path stroke="#633CFF" stroke-width="2" d="m1 1 6 6 6-6" />
+    </svg>
+  );
+}
