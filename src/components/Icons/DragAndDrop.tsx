83993dfab986aa05b81b02f0cc5b505f719fa740
Samuel Amaro <samuelamaro96746313@gmail.com>
2023-09-10 15:35:59 -0300

    add componente icon

---
+++ src/components/Icons/DragAndDrop.tsx
@@ -0,0 +1,16 @@
+import { PropsIcon } from "@/utils/utils";
+
+export default function DragAndDrop({ className }: PropsIcon) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="12"
+      height="6"
+      fill="none"
+      viewBox="0 0 12 6"
+      className={className}
+    >
+      <path fill="#737373" d="M0 0h12v1H0zM0 5h12v1H0z" />
+    </svg>
+  );
+}
