a41d79c0a597edee21d0ecd077ad5430b8c583dd
Samuel Amaro <samuelamaro96746313@gmail.com>
2023-09-23 16:04:27 -0300

    add class css e importação de styles

--- src/components/Empty/index.tsx
+++ src/components/Empty/index.tsx
@@ -1,16 +1,18 @@
 import Image from "next/image";
+import styles from "./styles.module.css";
 
 export default function Empty() {
   return (
-    <div>
+    <div className={styles.container}>
       <Image
         src="./images/illustration-empty.svg"
         alt="Ilustração demostrando contéudo vazio"
         width={250}
         height={161}
+        className={styles.ilustration}
       />
-      <h2>Vamos começar</h2>
-      <p>
+      <h2 className={`headingM ${styles.title}`}>Vamos começar</h2>
+      <p className={`bodyM ${styles.description}`}>
         Use o botão “Adicionar novo link” para começar. Depois de ter mais de um
         link, você pode reordená-los e editá-los. Estamos aqui para ajudá-lo a
         compartilhar seus perfis com todos!
