bb6bf892c54f209d6a7ee6c5de35c7c9f098ef32
Samuel Amaro <samuelamaro96746313@gmail.com>
2023-09-25 20:28:18 -0300

    removendo codigos comentados e comentarios

--- src/app/(linksProfile)/page.tsx
+++ src/app/(linksProfile)/page.tsx
@@ -1,13 +1,4 @@
-import styles from "./page.module.css";
-import Header from "@/components/Header";
-import MobileMockup from "@/components/MobileMockup";
 import dynamic from "next/dynamic";
-
-//TODO: INICIAR ESTILIZAÇÃO MOBILE-FIRST PAGE HOME E SEUS COMPONENTES
-//TODO: TERMINAR O COMPONENTE MOBILE-MOCKUP ADD OS CUSTOM LINKS PRONTOS, FOTO PROFILE OU INICIAIS, NOME, VER
-//TODO: OQUE FALTA PARA TERMINAR A PAGE HOME E ESTILIZAR
-//TODO: AO TERMINAR PAGE HOME IR PARA A PAGE PROFILE, DEPOIS IR PARA A PAGE VIEW
-//TODO: CONSTRUIR A PARTE INTERATIVA MAIS GROSSA DEPOIS ADD ESTILIZAÇÃO
 
 const CustomizeLinks = dynamic(
   () => import("../../components/CustomizeLinks/index"),
@@ -17,12 +8,5 @@ const CustomizeLinks = dynamic(
 export default function Home() {
   return (
     <CustomizeLinks />
-    /*<div className={styles.container}>
-      <Header />
-      <main className={styles.main}>
-        <MobileMockup />
-        <CustomizeLinks />
-      </main>
-    </div>*/
   );
 }
